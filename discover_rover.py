b2746f02bb8641e43606c5ebd7d6b19e27aaf18f
cjb873 <cjb873@nau.edu>
2022-05-10 11:19:58 -0700

    Refactored code to acutally run

--- discover_rover.py
+++ discover_rover.py
@@ -1,40 +1,36 @@
+#!/usr/bin/env python3
 import rospy
-from time import perf_counter
+from time import perf_counter 
 from geometry_msgs.msg import Twist
-
+from math import radians
 
 class Rover:
     def __init__(self, name):
         self.name = name
-        
-    def drive(linear_vel, angular_vel, duration):
+
+    def drive(self, linear_vel, angular_vel, duration):
+        rospy.init_node("drive_rover")
         twist = Twist()
         pub = rospy.Publisher("/cmd_vel", Twist, queue_size=20)
-        
+
         twist.linear.x = linear_vel
         twist.angular.z = angular_vel
         start_time = perf_counter()
-                
+
         while perf_counter() - start_time <= duration:
+            rospy.loginfo(time() - start_time)
             pub.publish(twist)
 
-    def drive(linear_vel, angular_vel):
-        drive(linear_vel, angular_vel, 1)
+    def move_forward(self, velocity, duration):
+        self.drive(velocity, 0, duration)
 
-    def move_forward(velocity, duration):
-        drive(velocity, 0, duration)
+    def move_backward(self, velocity, duration):
+        self.drive(-velocity, 0, duration)
 
-    def move_forward(velocity):
-        drive(distance, 0)
+    def turn_left(self, angle, duration):
+        angle_in_rad = radians(angle)
+        self.drive(0, angle_in_rad, duration)
 
-    def move_backward(velocity, duration):
-        drive(-velocity, 0, duration)
-
-    def move_backward(velocity):
-        drive(-velocity, 0)
-    
-    def turn_left(angle):
-        drive(0, angle)
-
-    def turn_right(angle):
-        drive(0, -angle)
+    def turn_right(self, angle, duration):
+        angle_in_rad = radians(angle)
+        self.drive(0, -angle_in_rad, duration)
