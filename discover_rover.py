dff38600fa3f68a0fed3cf61d4f4efe303c68d6f
cjb873 <cjb873@nau.edu>
2022-05-10 10:26:17 -0700

    Added functionality to drive and turn rover

--- discover_rover.py
+++ discover_rover.py
@@ -1,26 +1,40 @@
 import rospy
-
+from time import perf_counter
 from geometry_msgs.msg import Twist
 
 
 class Rover:
-    
-
     def __init__(self, name):
         self.name = name
-        rospy.init_node("rover", anonymous=True)
-
-    def move(linear, angular, iterations):
-        twist = Twist()
-        pub = rospy.Publisher("cmd_vel", Twist, queue_size=20)
-        twist.linear.x = linear
-        twist.angular.z = angular
-
-        for index in range(iterations):
-            pub.publish(twist)
-            rospy.sleep(0.1)
-
-
-    
-            
         
+    def drive(linear_vel, angular_vel, duration):
+        twist = Twist()
+        pub = rospy.Publisher("/cmd_vel", Twist, queue_size=20)
+        
+        twist.linear.x = linear_vel
+        twist.angular.z = angular_vel
+        start_time = perf_counter()
+                
+        while perf_counter() - start_time <= duration:
+            pub.publish(twist)
+
+    def drive(linear_vel, angular_vel):
+        drive(linear_vel, angular_vel, 1)
+
+    def move_forward(velocity, duration):
+        drive(velocity, 0, duration)
+
+    def move_forward(velocity):
+        drive(distance, 0)
+
+    def move_backward(velocity, duration):
+        drive(-velocity, 0, duration)
+
+    def move_backward(velocity):
+        drive(-velocity, 0)
+    
+    def turn_left(angle):
+        drive(0, angle)
+
+    def turn_right(angle):
+        drive(0, -angle)
