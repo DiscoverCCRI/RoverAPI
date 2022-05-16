472fb833388df3a31c85370fcdb39c9d0a9a6b2b
cjb873 <cjb873@nau.edu>
2022-05-15 18:48:16 -0700

    Updated to create an image buffer

--- camera_driver/discover_camera.py
+++ camera_driver/discover_camera.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from subprocess import check_output
-from re import findall
 import rospy
 import PIL.Image as Image
 from io import BytesIO
 from datetime import datetime
+from sensor_msgs.msg import CompressedImage
 
 
 class Camera:
@@ -16,7 +15,10 @@ class Camera:
 
     Attributes:
     -----------
-    none
+    img_buffer:
+        A list that contains the data of each image sent over the
+        /camera/image_raw/compressed topic. Each image is stored in an array of
+        unsigned 8-bit integers
 
     Methods:
     --------
@@ -24,58 +26,40 @@ class Camera:
         Creates the ros node to capture the image, captures the int[] from the
         compressed imaged posted to the compressed image topic, and converts it
         to a .jpg image for the user
-    __get_data_str() -> str:
-        Gets the output from the rostopic /camera/image_raw/compressed for a
-        single frame, and strips the output down to just the actual image data,
-        and returns that as a string
-    __str_to_int_list(in_str: str) -> []:
-        Takes a string and converts it to a list of integers that can be
-        interpreted as unsigned 8-bit integers for an image file, returns as an
-        integer list
+    __subscribe_to_image_topic():
+        Creates a subscribe to subscribe to the /camera/image_raw/compressed
+        topic created by the raspicam node. Then "spins" or continues to run
+        the function until the script is stopped
+    __callback_get_image(message: CompressedImage):
+        Gets the message from the /camera/image_raw/compressed topic and stores
+        the image data to the image buffer
     __list_to_img(int_list: []) -> img:
         Takes the list of unsigned 8-bit integers and converts it into an image
         object, then returns that image
     """
 
     def __init__(self):
-        pass
+        rospy.init_node("camera")
+        self.__subscribe_to_image_topic()
+        self.img_buffer = []
+
+    def __subscribe_to_image_topic(self):
+        subscriber = rospy.Subscriber("/camera/image_raw/compressed",
+                     CompressedImage, self.__callback_get_image)
+        rospy.spin()
+
+    def __callback_get_image(self, message: CompressedImage):
+        self.img_buffer.append(message.data)
 
     def take_photo(self):
-        rospy.init_node("raspicam_still_photo")
-
-        data_str = self.__get_data_str()
-        int_list = self.__str_to_int_list(data_str)
-        img = self.__list_to_img(int_list)
+        buffer_len = len(self.img_buffer)
+        latest_photo = self.img_buffer[buffer_len-1]
+        img = self.__list_to_img(latest_photo)
 
         time = datetime.now()
         time_str = "leo_cam_" + time.strftime("%d-%m-%Y_%H:%M:%S") + ".jpg"
 
         img.save(time_str)
-
-    def __get_data_str(self) -> str:
-        cmd_str = "rostopic echo -n 1 /camera/image_raw/compressed | grep data"
-        data = check_output(cmd_str, shell=True)
-        data = data.decode("utf-8")
-        data = data.lstrip("data: [")
-        data = data.rstrip()
-        data = data.rstrip("]")
-
-        return data
-
-    def __str_to_int_list(self, in_str: str) -> []:
-        str_len = len(in_str)
-        first_item = int(in_str[0:3])
-        last_item = int(in_str[str_len - 3:str_len])
-
-        found = findall("(?<=,)(.*?)(?=,)", in_str)
-
-        int_map = map(int, found)
-
-        int_list = list(int_map)
-        int_list.insert(0, first_item)
-        int_list.append(last_item)
-
-        return int_list
 
     def __list_to_img(self, img_list: []) -> Image:
         bytestring = bytearray()
