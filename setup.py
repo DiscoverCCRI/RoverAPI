9c1223a3283c56879584e4f05a4904f10d9f7e66
cjb873 <cjb873@nau.edu>
2022-05-17 11:56:48 -0700

    created setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['rover_api'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
+
