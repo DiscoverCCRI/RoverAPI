b92ddaf60736da4c3578b556661390ae55cd76cd
cjb873 <cjb873@nau.edu>
2022-06-15 14:46:53 -0700

    Created rover-setup.bash

---
+++ scripts/rover_setup.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+sudo apt-get update && sudo apt-get upgrade
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh \
+   && rm get-docker.sh
+cd ~/pi && git clone https://github.com/DiscoverCCRI/leorover-test-image.git
+cd ~/pi/leorover-test-image && docker build --network=host -t cjb873/leorover_image:1.0 .
+sudo echo "172.18.0.2 client" >> /etc/hosts
+cd /home/pi && git clone https://github.com/DiscoverCCRI/RoverAPI \
+    && mkdir -p catkin_ws/src && mv RoverAPI/rover_api catkin_ws/src
+    && cd catkin_ws && . /opt/ros/noetic/setup.bash && catkin_make \ 
+    && echo "/opt/ros/noetic/setup.bash" >> /home/pi/.bashrc \
+    && echo "/home/pi/catkin_ws/devel/setup.bash" >> /home/pi/.bashrc \
+    && mv /home/pi/RoverAPI/scripts/example.py /home/pi \
+    && chmod u+x /home/pi/example.py
+    && sudo rm -r /home/pi/RoverAPI
+sudo apt-get install cron
+crontab -l > mycron
+echo "@reboot sleep 20; sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\"" >> mycron
+crontab mycron && rm mycron
+
