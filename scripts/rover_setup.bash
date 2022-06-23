9b9269657a5c928d15493e9725eb4bab5bfa64ca
cjb873 <cjb873@nau.edu>
2022-06-22 19:28:24 -0700

    Fixed errors

--- scripts/rover_setup.bash
+++ scripts/rover_setup.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
+# First change to the superuser with the following command: sudo su
 sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+sudo timedatectl set-timezone America/Phoenix
 sudo apt-get update && sudo apt-get upgrade
+rosrun leo_fw update
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh \
    && rm get-docker.sh
 cd ~/pi && git clone https://github.com/DiscoverCCRI/leorover-test-image.git
@@ -9,12 +12,14 @@ cd ~/pi/leorover-test-image && docker build --network=host -t cjb873/leorover_im
 sudo echo "172.18.0.2 client" >> /etc/hosts
 cd /home/pi && git clone https://github.com/DiscoverCCRI/RoverAPI \
     && mkdir -p catkin_ws/src && mv RoverAPI/rover_api catkin_ws/src \
-    && cd catkin_ws && . /opt/ros/noetic/setup.bash && catkin_make \ 
-    && echo "/opt/ros/noetic/setup.bash" >> /home/pi/.bashrc \
-    && echo "/home/pi/catkin_ws/devel/setup.bash" >> /home/pi/.bashrc \
+    && cd catkin_ws && . /opt/ros/noetic/setup.bash && catkin_make
+echo "source /opt/ros/noetic/setup.bash" >> /home/pi/.bashrc \
+    && echo "source /home/pi/catkin_ws/devel/setup.bash" >> /home/pi/.bashrc \
     && mv /home/pi/RoverAPI/scripts/example.py /home/pi \
     && chmod u+x /home/pi/example.py \
     && sudo rm -r /home/pi/RoverAPI
 sudo apt-get install cron
-crontab -l | { cat; echo "@reboot sleep 20; sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\""; } | crontab -
 
+# Finally run the following command: crontab -e
+# Copy the following text into the crontab:
+# @reboot sleep 20; sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
