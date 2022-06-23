e13d91a31403272aa5d76f94c6085f24175a1393
Cody Beck <cjb873@nau.edu>
2022-06-23 10:28:14 -0700

    Update rover_setup.bash

--- scripts/rover_setup.bash
+++ scripts/rover_setup.bash
@@ -4,7 +4,7 @@
 sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
 sudo timedatectl set-timezone America/Phoenix
 sudo apt-get update && sudo apt-get upgrade
-rosrun leo_fw update
+. /opt/ros/$ROS_DISTRO/setup.bash && rosrun leo_fw update
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh \
    && rm get-docker.sh
 cd /home/pi && git clone https://github.com/DiscoverCCRI/leorover-test-image.git
