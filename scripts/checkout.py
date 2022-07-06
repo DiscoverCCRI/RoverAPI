0b4bac093df132aac3ff2d7fec3e1c2a35ad54ac
cjb873 <cjb873@nau.edu>
2022-07-06 10:56:02 -0700

    Changed to generate manifest

--- scripts/checkout.py
+++ scripts/checkout.py
@@ -3,18 +3,12 @@
 """
 Checkout
 
-This script uses the importlib module to install any necessary python modules
-on a docker container for the user. Any relevant .py files should be passed as
-command line arguments for the script to check for import statements. If the
-module cannot be found by pip, the name of the module is added to the pip.err
-file
+This script uses argv to scan any given files for imports and creates a list of
+modules that need to be installed using pip called a manifest
 """
 
 
-from importlib.util import find_spec
-from subprocess import run
-from sys import argv, modules
-from datetime import datetime
+from sys import argv
 from os.path import exists
 from os import remove
 
@@ -39,42 +33,25 @@ def find_modules(arguments: []) -> []:
                         import_modules.append(line)
                     elif 'import' in line:
                         line = line[7:]
-                        line = line[:line.find(' ')]
                         if '.' in line:
                             line = line[0:line.find('.')]
                         import_modules.append(line)
-                else:
-                    print("RoverAPI is already installed")
-    return modules
+    return import_modules
 
 
-def install_modules(import_modules: []):
-    for module in import_modules:
-        # check if the module is in the python path
-        if module in modules:
-            print(module + ' is in sys.modules')
-        # otherwise, check if the module has already been installed
-        elif find_spec(module) is not None:
-            print(module + ' is already installed')
-        # otherwise, try to install the module with pip
-        else:
-            try:
-                run('pip3 install ' + module, shell=True, check=True)
-            except Exception:
-                # write the name and the time of the module to pip.err
-                with open('pip.err', 'a') as outfile:
-                    time = datetime.now()
-                    time_str = time.strftime('%d-%m-%Y %H:%M:%S')
-                    out_str = time_str + ' Could not install ' + module + '.\n'
-                    outfile.write(out_str)
+def write_manifest(import_modules: []):
+    with open('manifest', 'w', encoding='utf-8') as outfile:
+        for module in import_modules:
+            outfile.write(module + "\n")
 
 
 def main():
-    if exists('pip.err'):
-        remove('pip.err')
+
+    if exists('manifest'):
+        remove('manifest')
 
     import_modules = find_modules(argv[1:])
-    install_modules(import_modules)
+    write_manifest(import_modules)
 
 
 if __name__ == '__main__':
