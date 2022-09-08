bc0a3fbd135ddf1af6b2b570265d80ded409c0e5
Cody Beck <cjb873@nau.edu>
2022-09-08 11:16:38 -0700

    Update discover_utils.py

--- rover_api/src/rover_api/discover_utils.py
+++ rover_api/src/rover_api/discover_utils.py
@@ -2,10 +2,12 @@ from rospy import Time
 from datetime import datetime
 
 
+DATA_DIR = "/experiment/"
+
 def get_time_str(time: Time, extension: str) -> str:
     # convert time to a python datetime object
     py_time = datetime.fromtimestamp(time.to_time())
 
     # convert time object to string
     time_str = py_time.strftime("%d-%m-%Y_%H:%M:%S")
-    return time_str + extension
+    return DATA_DIR + time_str + extension
