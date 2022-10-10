09aa38caa480ac336d87bb90456b625babc4d238
Cody Beck <cjb873@nau.edu>
2022-10-10 11:28:53 -0700

    Update discover_rover.py

--- rover_api/src/rover_api/discover_rover.py
+++ rover_api/src/rover_api/discover_rover.py
@@ -2,6 +2,7 @@ from math import radians
 from rospy import init_node, Publisher, get_time, loginfo
 from geometry_msgs.msg import Twist
 from rosbag import Bag
+from rover_api.discover_utils import get_time_str
 
 class Rover:
 
