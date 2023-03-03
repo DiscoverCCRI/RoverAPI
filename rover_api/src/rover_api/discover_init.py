384f50b0bfec7bbcb6770d4c93e840281b714e56
Cody Beck <cjb873@nau.edu>
2023-03-02 21:53:39 -0700

    Add set_callback

---
+++ rover_api/src/rover_api/discover_init.py
@@ -0,0 +1,17 @@
+from rospy import init_node, Subscriber
+from std_msgs.msg import Bool
+
+
+class ExperimentInitializer():
+    
+    def __init__(self, callback_func=None):
+        self.init_node("discover_experiment")
+        self.callback_func = callback_func
+        self.finished_sub = Subscriber("/finished", Bool, self._callback)
+
+    def _callback(self, msg):
+        if msg.data:
+            self.callback_func()
+     
+    def set_callback(self, callback_func):
+        self.callback_func = callback_func
