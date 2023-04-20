ed462907437d6524f67e2773f582c83daab8e420
Cody Beck <cjb873@nau.edu>
2023-04-20 11:15:24 -0700

    Change docstring to numpy format

--- rover_api/src/rover_api/discover_camera.py
+++ rover_api/src/rover_api/discover_camera.py
@@ -17,16 +17,32 @@ class Camera(Config):
     """
 
     def __init__(self, subscribe=True, callback=None):
-        """
-        @brief The constructor for the Camera class. Sets initial values
-        for attributes along with subscribing to image topic. Also creates
-        the experimental directories if needed.
-        @param self: The reference to the current object
-        @param subscribe: A boolean value indicating whether or not to
-        subscribe to the ROS image topic by default. Defaults to true.
-        @param callback: A function set to be called every time new image
-        data is published, and subscribe is True. Defaults to None.
-        @return: None
+        """ The constructor for the Camera class. 
+        
+        Sets initial values. for attributes along with subscribing to image 
+        topic. Also creates the experimental directories if needed.
+        
+        Parameters
+        ----------
+        subscribe : bool 
+            A boolean value indicating whether or not to subscribe to the ROS 
+            image topic by default. Defaults to true.
+        callback : function
+            A function set to be called every time new image
+            data is published, and subscribe is True. Defaults to None.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> def cb_func():
+        >>>     print("New images for cam2")
+        >>>
+        >>> cam1 = Camera()
+        >>> cam2 = Camera(False, cb_func)
         """
         try:
             loginfo("Camera initialized!")
@@ -46,26 +62,43 @@ class Camera(Config):
             super().__init__()
 
     def subscribe_to_image_topic(self):
-        """
-        @brief Subscribes to the ROS image topic published from the camera.
+        """Subscribes to the ROS image topic published from the camera.
+        
         If subscribe is set to False, call this function to get data from 
         the camera.
-        @param self: A reference to the current object.
-        @return: None
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> cam = Camera(False)
+        >>> cam.subscribe_to_image_topic()
         """
         Subscriber("/camera/image_raw", Image, self.__callback_get_image)
 
     def __callback_get_image(self, message: Image):
-        """
-        @brief A helper function that is used to actually get image data.
+        """A helper function that is used to actually get image data.
+        
         If the start_recording function has been called, this function will
         record image data to a rosbag file. If a callback function has been
         set, this function will call the callback. It appends the latest image
         data to an image buffer with a max length of 30 images.
-        @param self: A reference to the current object.
-        @param message: A sensor_msgs/Image object that corresponds to the
-        latest image.
-        @return: None
+        
+        Parameters
+        ----------
+        message : Image 
+            A sensor_msgs/Image object that corresponds to the latest image.
+            
+        Returns
+        -------
+        None
         """
         if self.callback_func is not None:
             self.callback_func()
@@ -81,22 +114,47 @@ class Camera(Config):
         self._img_buffer.append(message)
 
     def get_latest_image(self) -> Image:
-        """
-        @brief A function that returns the latest image in the form of 
+        """ A function that returns the latest image in the form of 
         a sensor_msgs/Image object.
-        @param self: A reference to the current object.
-        @return: The latest image in the form of a sensor_msgs/Image object.
+        
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        Image
+            The latest image in the form of a sensor_msgs/Image object.
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> from sensor_msgs.msg import Image
+        >>> cam = Camera()
+        >>> image1 = cam.get_latest_image()
         """
         return self._img_buffer[-1]
 
     def get_jpg(self):
-        """
-        @brief A function that uses OpenCV's CvBridge to convert a 
+        """ A function that uses OpenCV's CvBridge to convert a 
         sensor_msgs/Image object to an OpenCV image object, which is then
         saved as an .jpg file in the /experiment/photos directory. Images
-        are numbered sequentially from first to last.
-        @param self: A reference to the current object.
-        @return: None
+        are saved as /experiment/photos/<number_of_image>.jpg.
+        
+        Parameters
+        ----------
+        None        
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> cam = Camera()
+        >>> cam.get_jpg()
         """
         # create the bridge to translate image types
         bridge = CvBridge()
@@ -117,50 +175,117 @@ class Camera(Config):
         cv2.imwrite(img_str, img)
 
     def start_recording(self):
-        """
-        @brief A function that opens a new rosbag file to record all 
+        """A function that opens a new rosbag file to record all 
         ROS messages published on the /camera/image_raw topic.
-        @param self: A reference to the current object.
-        @return: None
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> cam = Camera()
+        >>> cam.start_recording()
         """
         self._bag_open = True
         self._bag = Bag(get_time_str(Time.now(), ".bag"), 'w')
 
     def stop_recording(self):
-        """
-        @brief A function that closes a previously opened rosbag file
+        """A function that closes a previously opened rosbag file
         that has records of ROS messages published on the /camera/image_raw
         topic.
-        @param self: A reference to the current object.
-        @return: None
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> from time import sleep
+        >>> cam = Camera()
+        >>> cam.start_recording()
+        >>> sleep(3)
+        >>> cam.stop_recording()
         """
         self._bag_open = False
         self._bag.close()
 
-    def isAvailable(self) -> Bool:
-        """
-        @brief A function that returns whether or not the camera on the 
+    def is_available(self) -> Bool:
+        """A function that returns whether or not the camera on the 
         rover is available.
-        @param self: A reference to the current object.
-        @return: A boolean value representing if the camera is available.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        bool
+            A boolean value representing if the camera is available.
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> cam = Camera()
+        >>> if cam.is_available():
+        >>>     cam.get_jpg()
         """
-        return super().isAvailable()
+        return super().is_available()
 
-    def getInfo(self) -> str:
-        """
-        @brief A function that returns information about the camera on the
+    def get_info(self) -> str:
+        """A function that returns information about the camera on the
         rover.
-        @param self: A reference to the current object.
-        @return: A string containing information about the camera.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        str
+            A string containing information about the camera.
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> cam = Camera()
+        >>> info = cam.get_info()
         """
-        return super().getInfo()
+        return super().get_info()
     
     def set_callback(self, func):
-        """
-        @brief A function that sets the callback function to be called
-        whenever new images from the camera are available
-        @param self: A reference to the current object.
-        @param func: The new callback function
-        @return: None
+        """A function that sets the callback function to be called
+        whenever new images from the camera are available. The new callback
+        function will not be called unless subscribe is set to True, or 
+        subscribe_to_image_topic() has been called.
+        
+        Parameters
+        ----------
+        func : function
+            The new callback function
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_camera import Camera
+        >>> def cb_func():
+        >>>     print("New image published")
+        >>> 
+        >>> cam = Camera(False, None)
+        >>> cam.set_callback(cb_func)
+        >>> cam.subscribe_to_image_topic()
         """
         self.callback_func = func
