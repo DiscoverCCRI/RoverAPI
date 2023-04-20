7a46d420b2b0f040975f69212fda7737e84af900
Cody Beck <cjb873@nau.edu>
2023-04-20 11:21:07 -0700

    Change docstrings to numpy format

--- rover_api/src/rover_api/discover_init.py
+++ rover_api/src/rover_api/discover_init.py
@@ -8,16 +8,31 @@ class ExperimentInitializer():
     """
     
     def __init__(self, callback_func=None):
-        """
-        @brief The constructor for the ExperimentInitializer class. It starts up
+        """The constructor for the ExperimentInitializer class. 
+        
+        It starts up
         the discover ROS node if one is not already in operation. It then sets a
         callback function which will be called when the user is finished with their
         experiment, or if the rover goes into low-power mode.
-        @param self: a reference to the current object.
-        @param callback_func: a user-defined function that will be called when the
-        experiment is over. It defaults to none, meaning no function will be called
-        when the experiment finishes.
-        @return: None
+        
+        Parameters
+        ----------
+        callback_func : function
+            A user-defined function that will be called when the
+            experiment is over. It defaults to none, meaning no function will be called
+            when the experiment finishes.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_init import ExperimentInitializer
+        >>> def experiment_done_cb():
+        >>>     print("Experiment is done!")
+        >>>
+        >>> inititalizer = ExperimentInitializer(experiment_done_cb)
         """
         try:
             init_node("discover")
@@ -26,23 +41,42 @@ class ExperimentInitializer():
             self.finished_sub = Subscriber("/finished", Bool, self._callback)
 
     def _callback(self, msg):
-        """
-        @brief A callback function for the subscriber created in the class constructor.
+        """A helper callback function for the subscriber created in the class constructor.
         If the boolean passed to this function is True, the experiment is over, so the
         function calls the user's callback function.
-        @param self: A reference to the current object.
-        @param msg: A ROS Bool message sent from a publisher on the topic /finished.
-        @return: None
+        
+        Parameters
+        ----------
+        msg : Bool
+            A ROS Bool message sent from a publisher on the topic /finished.
+        
+        Returns
+        -------
+        None
         """
         if msg.data:
             self.callback_func()
      
     def set_callback(self, callback_func):
-        """
-        @brief A function allowing the user to set their callback function at a time other
+        """A function allowing the user to set their callback function at a time other
         than construction of the object.
-        @param self: A reference to the current object.
-        @param callback_func: The new callback function to be used at the end of the experiment.
-        @return: None
+        
+        Parameters
+        ----------
+        callback_func : function 
+            The new callback function to be used at the end of the experiment.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_init import ExperimentInitializer
+        >>> def experiment_done_cb():
+        >>>     print("Experiment is done!")
+        >>>
+        >>> inititalizer = ExperimentInitializer()
+        >>> initializer.set_callback(experiment_done_cb)
         """
         self.callback_func = callback_func
