24b03d5542898b50d1b34c9fd9b8406fc6682185
Cody Beck <cjb873@nau.edu>
2023-04-20 12:01:16 -0700

    Change docstrings to numpy format

--- rover_api/src/rover_api/discover_rover.py
+++ rover_api/src/rover_api/discover_rover.py
@@ -15,15 +15,27 @@ class Rover(Config):
     """
 
     def __init__(self, subscribe=False, callback_func=None):
-        """
-        @brief The constructor for the Rover class. Sets initial values
-        for attributes.
-        @param self: The reference to the current object
-        @param subscribe: A boolean value indicating whether or not to
-        subscribe to the ROS cmd_vel topic by default. Defaults to False.
-        @param callback: A function set to be called every time the rover moves
-        and subscribe is True. Defaults to None.
-        @return: None
+        """The constructor for the Rover class. 
+        
+        Sets initial values for attributes.
+        
+        Parameters
+        ----------
+        subscribe : bool 
+            A boolean value indicating whether or not to subscribe to the ROS 
+            cmd_vel topic by default. Defaults to False.
+        callback : function 
+            A function set to be called every time the rover moves
+            and subscribe is True. Defaults to None.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
         """
         try:
             loginfo("Rover initialized!")
@@ -37,27 +49,44 @@ class Rover(Config):
                 makedirs("/experiment/photos/")
            
             self._rosbag = None
-            self.subscribe_to_vel()
+            self.subscribe_to_vel_topic()Update discover_lidar.py
             super().__init__() 
 
-    def subscribe_to_vel(self):
-        """
-        @brief This function subscribes to the cmd_vel topic. This function only needs
+    def subscribe_to_vel_topic(self):
+        """This function subscribes to the cmd_vel topic. This function only needs
         to be called if there is a callback that should be called whenever
         the rover moves and/or data needs to be saved to a rosbag. In either
-        event, this function must be called beforehand.
-        @param self: A reference to the current object.
-        @return: None
+        event, this function must be called beforehand,or subcribe must be set to
+        True in the constructor.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.subscribe_to_vel_topic()
         """
         Subscriber("/cmd_vel", Twist, self.__callback_get_vel)
         
     def __callback_get_vel(self, msg: Twist):
-        """
-        @brief This is a helper function to enable cmd_vel data to be written to an
+        """This is a helper function to enable cmd_vel data to be written to an
         open rosbag file or to enable a callback function.
-        @param self: A reference to the current object.
-        @param msg: A ROS Twist object that contains data about the rover's movement.
-        @return: None
+        
+        Parameters
+        ----------
+        msg : Twist
+            A ROS Twist object that contains data about the rover's movement.
+        
+        Returns
+        -------Update discover_lidar.py
+        None
         """
         if self.callback_func is not None:
             self.callback_func()
@@ -66,15 +95,29 @@ class Rover(Config):
             self._rosbag.write("/cmd_vel", msg)
        
     def drive(self, linear_vel: float, angular_vel: float, duration: float):
-        """
-        @brief This function allows the rover to drive, either linearly or angularly.
-        @param self: A reference to the current object.
-        @param linear_vel: The value in meters per second, of which the rover should move
-        linearly (forward or backward). The maximum accepted value is 0.4 meters per second.
-        @param angular_vel: The value in degrees per second, of which the rover should move
-        angularly (turn). The maximum accepted value is 60 degrees per second.
-        @param duration: The number of seconds for which the rover should execute given velocities.
-        @return: None
+        """ This function allows the rover to drive, either linearly or angularly.
+        
+        Parameters
+        ----------
+        linear_vel : float
+            The value in meters per second, of which the rover should move
+            linearly (forward or backward). The maximum accepted value is 0.4 meters per second.
+        angular_vel: float
+            The value in degrees per second, of which the rover should move
+            angularly (turn). The maximum accepted value is 60 degrees per second.
+        duration : float
+            The number of seconds for which the rover should execute given velocities.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.drive(0.15, 10, 10)
+        >>> rover.drive( -0.15, -10, 10)
         """
         twist = Twist()
         pub = Publisher("/cmd_vel", Twist, queue_size=20)
@@ -91,99 +134,206 @@ class Rover(Config):
             pub.publish(twist)
 
     def move_forward(self, velocity: float, duration: float):
-        """
-        @brief A function that allows the rover to move forward.
-        @param velocity: The value in meters per second, for which the
-        rover should move forward. Negative values will move the rover
-        in reverse. The maximum accepted value is 0.4 meters per second.
-        @param duration: The number of seconds for which the rover should
-        drive forward.
-        @return: None
+        """A function that allows the rover to move forward.
+        
+        Parameters
+        ----------
+        velocity : float
+            The value in meters per second, for which the
+            rover should move forward. Negative values will move the rover
+            in reverse. The maximum accepted value is 0.4 meters per second.
+        duration : float
+            The number of seconds for which the rover should drive forward.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.move_forward(.10, 10)
         """
         self.drive(velocity, 0, duration)
 
     def move_backward(self, velocity: float, duration: float):
-        """
-        @brief A function that allows the rover to move backward.
-        @param velocity: The value in meters per second, for which the rover
-        should move in reverse. Negative value will move the rover forward.
-        The minimum accepted value is -0.4 meters per second.
-        @param duration: The number of seconds for which the rover should 
-        drive backward.
-        @return: None
+        """A function that allows the rover to move backward.
+        
+        Parameters
+        ----------
+        velocity : float
+            The value in meters per second, for which the rover
+            should move in reverse. Negative value will move the rover forward.
+            The minimum accepted value is -0.4 meters per second.
+        duration : floatUpdate discover_lidar.py
+            The number of seconds for which the rover should drive backward.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.move_backward(.10, 10)
         """
         self.drive(-velocity, 0, duration)
 
     def turn_left(self, angle: float, duration: float):
-        """
-        @brief A function that allows the rover to turn left.
-        @param velocity: The value in degrees per second, for which the rover
-        should turn left. Negative value turn the rover to the right.
-        The minimum accepted value is 60 degrees per second.
-        @param duration: The number of seconds for which the rover should 
-        turn left.
-        @return: None
+        """A function that allows the rover to turn left.
+        
+        Parameters
+        ----------
+        velocity : float 
+            The value in degrees per second, for which the rover
+            should turn left. Negative value turn the rover to the right.
+            The minimum accepted value is 60 degrees per second.
+        duration : float
+            The number of seconds for which the rover should turn left.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.turn_left(10, 10)
         """
         self.drive(0, angle, duration)
 
     def turn_right(self, angle: float, duration: float):
-         """
-        @brief A function that allows the rover to turn right.
-        @param velocity: The value in degrees per second, for which the rover
-        should turn right. Negative value turn the rover to the left.
-        The minimum accepted value is 60 degrees per second.
-        @param duration: The number of seconds for which the rover should 
-        turn right.
-        @return: None
+        """A function that allows the rover to turn right.
+        
+        Parameters
+        ----------
+        velocity : float 
+            The value in degrees per second, for which the rover
+            should turn right. Negative value turn the rover to the left.
+            The minimum accepted value is 60 degrees per second.
+        duration : float
+            The number of seconds for which the rover should turn right.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.turn_right(10, 10)
         """
         self.drive(0, -angle, duration)
         
     def start_recording(self):
-        """
-        @brief A function that opens a new rosbag file to record all 
-        ROS messages published on the /cmd_vel topic.
-        @param self: A reference to the current object.
-        @return: None
+        """A function that opens a new rosbag file to record all 
+        ROS messages published on the /cmd_vel topic. For this to work
+        it must be called in conjunction with subscribe_to_vel_topic(),
+        or subscribe must be set to True in the constructor.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.subscribe_to_vel_topic()
+        >>> rover.start_recording()Update discover_lidar.py
         """
         self._bag_open = True
         self._rosbag = Bag(get_time_str(Time.now(), "_vel.bag"), 'w')
 
     def stop_recording(self):
-        """
-        @brief A function that closes a previously opened rosbag file
+        """A function that closes a previously opened rosbag file
         that has records of ROS messages published on the /cmd_vel
         topic.
-        @param self: A reference to the current object.
-        @return: None
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> rover.subscribe_to_vel_topic()
+        >>> rover.start_recording()
+        >>> rover.move_forward(.10, 10)
+        >>> rover.stop_recording()
         """
         self._bag_open = False
         self._rosbag.close()
 
-    def getInfo(self):
+    def get_info(self):
+        """A function that returns information about the rover.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> rover = Rover()
+        >>> info = rover.get_info()
         """
-        @brief A function that returns information about the 
-        rover.
-        @param self: A reference to the current object.
-        @return: A string containing information about the rover.
-        """
-        return super().getInfo()
+        return super().get_info()
 
-    def __isAvailable(self):
+    def __is_available(self):
+        """A function that returns whether or not the rover is available.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        bool 
+            A boolean value representing if the rover is available.
         """
-        @brief A function that returns whether or not the 
-        rover is available.
-        @param self: A reference to the current object.
-        @return: A boolean value representing if the rover is available.
-        """
-        return super().isAvailable()
+        return super().is_available()
     
      def set_callback(self, func):
-        """
-        @brief A function that sets the callback function to be called
-        whenever the rover moves
-        @param self: A reference to the current object.
-        @param func: The new callback function
-        @return: None
+        """A function that sets the callback function to be called whenever the rover moves.
+        For the callback to be called, subscribe must be set to True in the constructor, or
+        subscribe_to_vel_topic() must have been previously called.
+        
+        Parameters
+        ----------
+        func : function
+            This is the new callback function.
+        
+        Returns
+        -------
+        None
+        
+        Examples
+        --------
+        >>> from rover_api.discover_rover import Rover
+        >>> def cb_func():
+        >>>     print("The rover is moving")
+        >>>
+        >>> rover = Rover()
+        >>> rover.set_callback(cb_func)
+        >>> rover.subscribe_to_vel_topic()
         """
         self.callback_func = func
 
